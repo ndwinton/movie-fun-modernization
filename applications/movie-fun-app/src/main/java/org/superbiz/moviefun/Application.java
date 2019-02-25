e0c71ed341c4652889680e93aed4f4af351b6bc1
Neil Winton <nwinton@pivotal.io>
2019-02-25 13:13:35 +0530

    Refactored albums component

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
@@ -8,8 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.superbiz.moviefun.blobstore.BlobStore;
-import org.superbiz.moviefun.blobstore.S3Store;
+import org.superbiz.moviefun.albums.blobstore.BlobStore;
+import org.superbiz.moviefun.albums.blobstore.S3Store;
 import org.superbiz.moviefun.movies.MovieServlet;
 
 @SpringBootApplication
