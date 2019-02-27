6648edb2c313c81aef46c14c0939722ea410f3b3
Neil Winton <nwinton@pivotal.io>
2019-02-27 13:26:56 +0530

    Use configuration from environment, not hard-coded

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
@@ -1,5 +1,6 @@
 package org.superbiz.moviefun;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
@@ -9,13 +10,18 @@ import org.superbiz.moviefun.moviesapi.MoviesClient;
 @Configuration
 public class ClientConfiguration {
 
+    @Value("${albums.url}") String albumsUrl;
+    @Value("${movies.url}") String moviesUrl;
+
     @Bean
     public AlbumsClient albumsClient(RestOperations restOperations) {
-        return new AlbumsClient("//album-service/albums", restOperations);
+        System.err.println("albumsUrl = " + albumsUrl);
+        return new AlbumsClient(albumsUrl, restOperations);
     }
 
     @Bean
     public MoviesClient moviesClient(RestOperations restOperations) {
-        return new MoviesClient("//movie-service/movies", restOperations);
+        System.err.println("moviesUrl = " + moviesUrl);
+        return new MoviesClient(moviesUrl, restOperations);
     }
 }
